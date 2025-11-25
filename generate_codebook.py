002fa0d6c0f6f0ea385277c1d95f08eee3e09a77
Martinez-Cardoso <areshamc@bsdad.uchicago.edu>
2025-11-24 23:33:32 -0600

    finished codebook

--- generate_codebook.py
+++ generate_codebook.py
@@ -1,36 +1,30 @@
-data = [  ("Economic and Employment", "Fair Market Rent", "ho7_FIPS_2010", "float64", "Fips code for 2010"),
-    ("Economic and Employment", "Fair Market Rent", "ho7_STNAME", "object", "State name"),
-    ("Economic and Employment", "Fair Market Rent", "ho7_countyname", "object", "County name"),
-    ("Economic and Employment", "Fair Market Rent", "ho7_fmr_0", "int64", "Fair Market Rent for an Efficiency"),
-    ("Economic and Employment", "Fair Market Rent", "ho7_fmr_1", "int64", "Fair Market Rent for a 1-bedroom unit"),
-    ("Economic and Employment", "Fair Market Rent", "ho7_fmr_2", "int64", "Fair Market Rent for a 2-bedroom unit"),
-    ("Economic and Employment", "Fair Market Rent", "ho7_fmr_3", "int64", "Fair Market Rent for a 3-bedroom unit"),
-    ("Economic and Employment", "Fair Market Rent", "ho7_fmr_4", "int64", "Fair Market Rent for a 4-bedroom unit"),
-    ("Economic and Employment", "Fair Market Rent", "ho7_pop2010", "float64", "2010 Census population"),
-    ("Economic and Employment", "Fair Market Rent", "ho7_fmr_type", "int64", "40th/50th percentile indicator"),]
-    
-data2 = [("Economic and Employment", "USA Spending", "ec9_FIPS_2010", "int64", "Fips code for 2010"),
-    ("Economic and Employment", "USA Spending", "ec9_State", "object", "State name"),
-    ("Economic and Employment", "USA Spending", "ec9_CTYNAME", "object", "County name"),
-    ("Economic and Employment", "USA Spending", "ec9_Total_Prime_Award", "float64", "Total prime award count"),
-    ("Economic and Employment", "USA Spending", "ec9_Total_Sub_Award", "float64", "Total subaward count"),]
+data = [
+    ("Housing", "Mortgage Lending", "int64", "ho2_NUM_ORIGINATIONS"),
+    ("Housing", "Mortgage Lending", "int64", "ho2_AMT_ORIGINATIONS"),
+    ("Housing", "Mortgage Lending", "int64", "ho2_NUM_CONVENTIONAL"),
+    ("Housing", "Mortgage Lending", "int64", "ho2_AMT_CONVENTIONAL"),
+    ("Housing", "Mortgage Lending", "int64", "ho2_NUM_GOVERNMENT"),
+    ("Housing", "Mortgage Lending", "int64", "ho2_AMT_GOVERNMENT"),
+    ("Housing", "Mortgage Lending", "int64", "ho2_NUM_FHA"),
+    ("Housing", "Mortgage Lending", "int64", "ho2_AMT_FHA"),
+    ("Housing", "Mortgage Lending", "int64", "ho2_NUM_VA"),
+    ("Housing", "Mortgage Lending", "int64", "ho2_AMT_VA"),
+    ("Housing", "Mortgage Lending", "int64", "ho2_NUM_RHS_FSA"),
+    ("Housing", "Mortgage Lending", "int64", "ho2_AMT_RHS_FSA"),
+    ("Housing", "Mortgage Lending", "int64", "ho2_NUM_HOME_PURCHASE"),
+    ("Housing", "Mortgage Lending", "int64", "ho2_AMT_HOME_PURCHASE"),
+    ("Housing", "Mortgage Lending", "int64", "ho2_NUM_HOME_IMPROVEMENT"),
+    ("Housing", "Mortgage Lending", "int64", "ho2_AMT_HOME_IMPROVEMENT"),
+    ("Housing", "Mortgage Lending", "int64", "ho2_NUM_REFINANCE"),
+    ("Housing", "Mortgage Lending", "int64", "ho2_AMT_REFINANCE"),
+]
 
-for category, subcategory, var, type, description in data:
+for category, subcategory, data, var, in data:
     print(f"""<tr data-domain="Housing">
         <td><a href="#" onclick="goToDomainSection('Housing')">{category}</a></td>
         <td>{subcategory}</td>
-        <td>HUD Fair Market Rent (2000-2025)</td> 
+        <td>NaNDA (1981-2024) Home Mortgage Disclosure Act</td>
         <td>{var}</td>
-        <td>{type}</td>
-        <td>{description}</td>
-        </tr>""")
-    
-for category2, subcategory2, var2, type2, description2 in data2:
-    print(f"""<tr data-domain="Economics">
-        <td><a href="#" onclick="goToDomainSection('Economics')">{category2}</a></td>
-        <td>{subcategory2}</td>
-        <td>Custom Award Data</td> 
-        <td>{var2}</td>
-        <td>{type2}</td>
-        <td>{description2}</td>
+        <td>{data}</td>
+        <td></td>
         </tr>""")
